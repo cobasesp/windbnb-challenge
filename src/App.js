5890f24dfd69f3aee2534b62df039e5ab810af0c
David Cobas <cobasesp.developer@gmail.com>
2021-08-10 21:24:23 +0200

    Project ready to start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './app.scss';
+import {useEffect, useState} from 'react';
+import data from './stays.json';
 
 function App() {
+
+  const [hotelList, setHotels] = useState([]);
+
+  // Load data
+  useEffect(() => {
+    setHotels(data);
+  }, []);
   return (
     <div>
       windbnb app
+
+      {hotelList.map((hotel) => (
+        <p>{hotel.title}</p>
+      ))}
     </div>
   );
 }
